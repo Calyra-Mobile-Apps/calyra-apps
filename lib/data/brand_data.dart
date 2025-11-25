1bbd1edb9872196401a47066d23c35691faabb36
kaylakusnadi <kaylakusnadi111@gmail.com>
2025-11-25 09:41:19 +0700

    fix home pict and logo

--- lib/data/brand_data.dart
+++ lib/data/brand_data.dart
@@ -6,7 +6,7 @@ final List<BrandInfo> featuredBrands = [
   const BrandInfo(
     id: 'BRD001',
     name: 'Wardah',
-    imageUrl: 'assets/images/homew.png',
+    imageUrl: 'assets/images/homew.PNG',
     logoPath: 'assets/images/Logo_wardah.png',
     homeLogoPath: 'assets/images/wardah-white.png',
     products: [],
@@ -14,7 +14,7 @@ final List<BrandInfo> featuredBrands = [
   const BrandInfo(
     id: 'BRD002',
     name: 'Emina',
-    imageUrl: 'assets/images/homee.png',
+    imageUrl: 'assets/images/homee.PNG',
     logoPath: 'assets/images/Logo_emina.png',
     homeLogoPath: 'assets/images/emina-white.png',
     products: [],
@@ -22,7 +22,7 @@ final List<BrandInfo> featuredBrands = [
   const BrandInfo(
     id: 'BRD007',
     name: 'Make Over',
-    imageUrl: 'assets/images/homem.png',
+    imageUrl: 'assets/images/homem.PNG',
     logoPath: 'assets/images/Logo_makeover.png',
     homeLogoPath: 'assets/images/makeover-white.png',
     products: [],
@@ -30,7 +30,7 @@ final List<BrandInfo> featuredBrands = [
   const BrandInfo(
     id: 'BRD006',
     name: 'Somethinc',
-    imageUrl: 'assets/images/homes.png',
+    imageUrl: 'assets/images/homes.PNG',
     logoPath: 'assets/images/Logo_somethinc.png',
     homeLogoPath: 'assets/images/somethinc-white.png',
     products: [],
@@ -38,7 +38,7 @@ final List<BrandInfo> featuredBrands = [
   const BrandInfo(
     id: 'BRD005',
     name: 'OMG Beauty',
-    imageUrl: 'assets/images/homeo.png',
+    imageUrl: 'assets/images/homeo.PNG',
     logoPath: 'assets/images/Logo_OMG.png',
     homeLogoPath: 'assets/images/omg-white.png',
     products: [],
@@ -46,7 +46,7 @@ final List<BrandInfo> featuredBrands = [
   const BrandInfo(
     id: 'BRD003',
     name: 'Luxcrime',
-    imageUrl: 'assets/images/homel.png',
+    imageUrl: 'assets/images/homel.PNG',
     logoPath: 'assets/images/Logo_luxcrime.png',
     homeLogoPath: 'assets/images/luxcrime-white.png',
     products: [],
